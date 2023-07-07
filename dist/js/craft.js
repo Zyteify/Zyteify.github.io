447890bd33f5d8c58de544f114d8c5b80a4cfa8d
Zyteify <Zyteify1@gmail.com>
2023-07-07 18:18:54 +0800

    refactor item/craft

--- dist/js/craft.js
+++ dist/js/craft.js
@@ -10,6 +10,282 @@ let craftingItemMaterials = document.createElement('p');
 craftingItemMaterials.id = "crafting-item-materials";
 craftingItemMaterials.innerHTML = "Item Materials";
 craftingMaterialsListDiv.appendChild(craftingItemMaterials);
+const itemTypes = ["Weapon", "Boot"];
+const gearTypes = [
+    "Hoe",
+    //"Mace",
+    "Potion",
+    "Hammer",
+    //"Knife",
+    "Bow",
+    "Chisel",
+    "Quill",
+    "Dice",
+    //"Scales",
+    //"Holy Symbol",
+    //"Scroll",
+    "Pickaxe",
+    "Axe",
+];
+const gearBaseTypeMap = {
+    'Hoe': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Mace': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Potion': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Hammer': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Knife': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Bow': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Chisel': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Quill': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Dice': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Scales': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Holy Symbol': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Scroll': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Pickaxe': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+    'Axe': ['Wooden', 'Copper', 'Bronze', 'Iron', 'Gold'],
+};
+const baseTypeResourceCostMap = {
+    'Wooden': 5,
+    'Copper': 10,
+    'Bronze': 100,
+    'Iron': 100,
+    'Gold': 100,
+};
+const baseTypeCraftingCostMap = {
+    'Wooden': 5,
+    'Copper': 10,
+    'Bronze': 100,
+    'Iron': 1000,
+    'Gold': 10000,
+};
+const vocationMap = {
+    'Hoe': 'Farmer',
+    'Mace': 'Guard',
+    'Potion': 'Nurse',
+    'Hammer': 'Blacksmith',
+    'Knife': 'Cook',
+    'Bow': 'Hunter',
+    'Chisel': 'Gemcutter',
+    'Quill': 'Taxer',
+    'Dice': 'Gambler',
+    'Scales': 'Taxer',
+    'Holy Symbol': 'Priest',
+    'Scroll': 'Researcher',
+    'Pickaxe': 'Miner',
+    'Axe': 'Woodcutter',
+};
+let rarityChanceCommon = 0.8;
+let rarityChanceMagic = 0.5;
+let rarityChanceRare = 0.2;
+let rarityChanceUnique = 0.00;
+let baseTypeChanceWooden = 0.8;
+let baseTypeChanceCopper = 0.5;
+let baseTypeChanceBronze = 0.2;
+let baseTypeChanceIron = 0.05;
+let baseTypeChanceGold = 0.01;
+function generateRarity() {
+    let rarity = "Common";
+    let totalChance = rarityChanceCommon + rarityChanceMagic + rarityChanceRare + rarityChanceUnique;
+    let random = Math.random() * totalChance;
+    //generate a random number between 0 and 
+    switch (true) {
+        case (random < rarityChanceCommon):
+            rarity = 'Common';
+            break;
+        case (random < rarityChanceCommon + rarityChanceMagic):
+            rarity = 'Magic';
+            break;
+        case (random < rarityChanceCommon + rarityChanceMagic + rarityChanceRare):
+            rarity = 'Rare';
+            break;
+        case (random < rarityChanceCommon + rarityChanceMagic + rarityChanceRare + rarityChanceUnique):
+            rarity = 'Unique';
+            break;
+        default:
+            rarity = 'Common';
+            console.log('error chances in generateRarity');
+            break;
+    }
+    return rarity;
+}
+function generateBaseType() {
+    let baseType = "Wooden";
+    let totalChance = baseTypeChanceWooden + baseTypeChanceCopper + baseTypeChanceBronze + baseTypeChanceIron + baseTypeChanceGold;
+    let random = Math.random() * totalChance;
+    //generate a random number between 0 and 
+    switch (true) {
+        case (random < baseTypeChanceWooden):
+            baseType = 'Wooden';
+            break;
+        case (random < baseTypeChanceWooden + baseTypeChanceCopper):
+            baseType = 'Copper';
+            break;
+        case (random < baseTypeChanceWooden + baseTypeChanceCopper + baseTypeChanceBronze):
+            baseType = 'Bronze';
+            break;
+        case (random < baseTypeChanceWooden + baseTypeChanceCopper + baseTypeChanceBronze + baseTypeChanceIron):
+            baseType = 'Iron';
+            break;
+        case (random < baseTypeChanceWooden + baseTypeChanceCopper + baseTypeChanceBronze + baseTypeChanceIron + baseTypeChanceGold):
+            baseType = 'Gold';
+            break;
+        default:
+            baseType = 'Wooden';
+            console.log('error chances in generateRarity');
+            break;
+    }
+    return baseType;
+}
+function generateGear() {
+    let gear = "Hoe";
+    let random = Math.random() * gearTypes.length;
+    //set gear to a random gear type
+    gear = gearTypes[Math.floor(random)];
+    return gear;
+}
+function rollRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let chanceStatMin = 0.5;
+let chanceStatMax = 0.5;
+function generateAffixes(rarity, gear) {
+    let stats = [];
+    /* stats.push(stat); */
+    let numPrefixes = 0;
+    let numSuffixes = 0;
+    let totalChance = chanceStatMin + chanceStatMax;
+    let random = Math.random() * totalChance;
+    //get the number of stats to generate
+    switch (rarity) {
+        case "Starter":
+            console.log(`shouldnt be generating stats for start gear`);
+            break;
+        case "Common":
+            numPrefixes = 0;
+            numSuffixes = 0;
+            break;
+        case "Magic":
+            if (random < chanceStatMin) {
+                if (Math.random() < 0.5) {
+                    numPrefixes = 1;
+                    numSuffixes = 0;
+                }
+                else {
+                    numPrefixes = 0;
+                    numSuffixes = 1;
+                }
+            }
+            else {
+                numPrefixes = 1;
+                numSuffixes = 1;
+            }
+            break;
+        case "Rare":
+            if (random < chanceStatMin) {
+                if (Math.random() < 0.5) {
+                    numPrefixes = 2;
+                    numSuffixes = 1;
+                }
+                else {
+                    numPrefixes = 1;
+                    numSuffixes = 2;
+                }
+            }
+            else {
+                numPrefixes = 2;
+                numSuffixes = 2;
+            }
+            break;
+        case "Unique":
+            console.log(`shouldnt be generating stats for unique gear`);
+            break;
+        default:
+            console.log(`error in generateAffixes`);
+            break;
+    }
+    //generate the stats
+    for (let i = 0; i < numPrefixes; i++) {
+        //get the list of affixes that are prefixes
+        let prefixList = affixList.filter(affix => affix.affix == "Prefix");
+        //get the list of affixes that are prefixes with the same gear type
+        let prefixGearList = prefixList.filter(affix => affix.gearType == gear || affix.gearType == 'ANY');
+        //get a single prefix from the list using the affix weights
+        let prefix = getAffix(prefixGearList, stats);
+        //roll the value of the stat
+        let value = rollRange(prefix.valueMin, prefix.valueMax);
+        //create the stat
+        let stat = {
+            name: prefix.name,
+            valueRange: [prefix.valueMin, prefix.valueMax],
+            value: value,
+            stat: prefix.stat,
+            affix: prefix.affix,
+            tier: prefix.tier,
+            modFamily: prefix.modFamily,
+            tags: prefix.tags,
+        };
+        stats.push(stat);
+    }
+    for (let i = 0; i < numSuffixes; i++) {
+        //get the list of affixes that are suffixes
+        let SuffixList = affixList.filter(affix => affix.affix == "Suffix");
+        //get the list of affixes that are suffixes with the same gear type
+        let suffixGearList = SuffixList.filter(affix => affix.gearType == gear || affix.gearType == 'ANY');
+        //get a single suffix from the list using the affix weights
+        let suffix = getAffix(suffixGearList, stats);
+        //roll the value of the stat
+        let value = rollRange(suffix.valueMin, suffix.valueMax);
+        //create the stat
+        let stat = {
+            name: suffix.name,
+            valueRange: [suffix.valueMin, suffix.valueMax],
+            value: value,
+            stat: suffix.stat,
+            affix: suffix.affix,
+            tier: suffix.tier,
+            modFamily: suffix.modFamily,
+            tags: suffix.tags,
+        };
+        stats.push(stat);
+    }
+    return stats;
+}
+let showFullItems = true;
+//create button to switch from short to long display
+const displayButton = document.createElement('button');
+displayButton.id = "display-button";
+displayButton.className = "display-button";
+displayButton.draggable = false;
+displayButton.innerHTML = "Show Full Items";
+displayButton.onclick = function () {
+    if (showFullItems) {
+        displayButton.innerHTML = "Show Short Items";
+    }
+    else {
+        displayButton.innerHTML = "Show Full Items";
+    }
+    showFullItems = !showFullItems;
+    displayGear();
+};
+if (showFullItems) {
+    displayButton.innerHTML = "Show Short Items";
+}
+else {
+    displayButton.innerHTML = "Show Full Items";
+}
+//attach the button to the gear list
+const gearList = document.getElementById('gear');
+gearList.appendChild(displayButton);
+function removeAllChildren(node) {
+    while (node.firstChild) {
+        removeAllChildren(node.firstChild);
+        node.removeChild(node.firstChild);
+    }
+}
+//add a delete div to the gear div
+deleteDiv.id = "delete-div";
+deleteDiv.className = "delete-div";
+deleteDiv.draggable = false;
+deleteDiv.innerHTML = "Delete";
 function createRandomGear() {
     let randomGearType = gearTypes[Math.floor(Math.random() * gearTypes.length)];
     let randomItemType = itemTypes[Math.floor(Math.random() * itemTypes.length)];
